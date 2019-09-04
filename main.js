0aeed680128bdb9f3351ce8abd510c43565bde41
Akuagwu Philemon <45084023+PhilzAce@users.noreply.github.com>
2019-09-04 18:07:47 +0100

    added validation

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 
 function validate() {
-    if (document.email - form.name.value == "" || document.email - form.name.value < 4) {
+    if (document.form.name.value == "" || document.email - form.name.value < 4) {
         alert("Please provide your name! and make sure it is up to four");
-        document.email - form.name.focus();
+        document.form.name.focus();
         return false;
     }
-    if (document.email - form.email.value == "" || document.email - form.email.value < 4) {
+    
+    if (document.form.email.value == "" || document.email - form.email.value < 4) {
         alert("Please provide your Email!");
-        document.email - form.email.focus();
-        return false;
-    }
-
-    if (document.email - form.title.value == "" || document.email - form.title.value < 4) {
-        alert("Please provide your Email!");
-        document.email - form.title.focus();
+        document.form.email.focus();
         return false;
     }
 
-    if (document.email - form.email.message == "" || document.email - form.name.value < 20) {
+    if (document.form.title.value == "" || document.email - form.title.value < 4) {
         alert("Please provide your Email!");
-        document.email - form.message.focus();
+        document.form.title.focus();
+        return false;
+    }
+
+    if (document.email.form.email.message == "" || document.email - form.name.value < 20) {
+        alert("Please provide your Email!");
+        document.form.message.focus();
         return false;
     }
 
